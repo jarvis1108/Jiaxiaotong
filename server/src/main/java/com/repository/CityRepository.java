7b7ed716f358f890d849902a143cdf8732486191
Jarvis <940179935@qq.com>
2019-05-11 22:44:53 +0800

    update

--- server/src/main/java/com/repository/CityRepository.java
+++ server/src/main/java/com/repository/CityRepository.java
@@ -4,4 +4,5 @@ import com.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City,Integer> {
+
 }
--- server/src/main/java/com/repository/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import com.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserID(String userid);
-}
-
